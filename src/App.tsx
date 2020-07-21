e7b285c7f00cf3acea3cca2afb3ac92876f1887e
kakitaku00 <webtrakakinuma@gmail.com>
2020-07-21 12:08:40 +0900

    component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import PostCard from "./components/PostCard";
+import PostList from "./components/PostList";
 import "./App.css";
 
-import postsApi from "./api/postsApi";
-
-type Post = {
-  title: string;
-  body: string;
-  imageUrl?: string;
-};
-
 const App: React.FC = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    postsApi.get("/").then((res) => {
-      setPosts(res.data.posts);
-    });
-  }, []);
-
-  if (!posts) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <div className="App">
       <Header />
-      {posts.map((post: Post, index: number) => (
-        <PostCard
-          title={post.title}
-          body={post.body}
-          imageUrl={
-            "https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2450&q=80"
-          }
-          key={index.toString()}
-        />
-      ))}
+      <PostList />
       <Footer />
     </div>
   );
---
+++ src/components/PostList.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import PostCard from "./PostCard";
+import postsApi from "../api/postsApi";
+
+type Post = {
+  title: string;
+  body: string;
+  imageUrl?: string;
+};
+
+const PostList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    postsApi.get("/").then((res) => {
+      setPosts(res.data.posts);
+    });
+  }, []);
+
+  if (!posts.length) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      {posts.map((post: Post, index: number) => (
+        <PostCard
+          title={post.title}
+          body={post.body}
+          imageUrl={
+            "https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2450&q=80"
+          }
+          key={index.toString()}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
