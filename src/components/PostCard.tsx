35ad1ac2860dad4ef1611fdd5479bc634f28e4c6
kakitaku00 <webtrakakinuma@gmail.com>
2020-07-21 12:29:07 +0900

    add tag

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import Chip from "@material-ui/core/Chip";
 
 type Post = {
   title: string;
@@ -12,14 +13,24 @@ type Post = {
   imageUrl?: string;
 };
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-});
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+    chipWrapper: {
+      display: "flex",
+      flexWrap: "wrap",
+      marginTop: "1rem",
+      "& > *:not(:first-of-type)": {
+        marginLeft: "4px",
+      },
+    },
+  })
+);
 
 const PostCard = ({ title, body, imageUrl }: Post) => {
   const classes = useStyles();
@@ -35,6 +46,10 @@ const PostCard = ({ title, body, imageUrl }: Post) => {
           <Typography variant="body2" color="textSecondary" component="p">
             {body.length >= 30 ? body.substr(0, 30) + "..." : body}
           </Typography>
+          <div className={classes.chipWrapper}>
+            <Chip label="FrontEnd" />
+            <Chip label="Blog" />
+          </div>
         </CardContent>
       </CardActionArea>
     </Card>
