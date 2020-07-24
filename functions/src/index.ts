b285a9b13ec17ded09f93e1569c2575a4fe2fc51
kakitaku00 <webtrakakinuma@gmail.com>
2020-07-24 16:51:48 +0900

    functions delete comment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,28 +1,10 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
-// import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import postsRouter from "./routers/articleRouter";
 
-// const options: cors.CorsOptions = {
-//   allowedHeaders: [
-//     "Origin",
-//     "X-Requested-With",
-//     "Content-Type",
-//     "Accept",
-//     "X-Access-Token",
-//     "Authorization",
-//   ],
-//   credentials: true,
-//   methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-//   origin: "*",
-//   preflightContinue: false,
-// };
-
 const app = express();
 app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
 
 app.use("/api/v1", postsRouter);
 
