5ad0adebd9b6a63c82104732d2dbf805c2a17335
kakitaku00 <webtrakakinuma@gmail.com>
2020-07-28 12:48:50 +0900

    エンドポイントURL名を修正

--- src/api/articleApi.ts
+++ src/api/articleApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiURL = process.env.REACT_APP_API_URL;
 
 const articleApi = axios.create({
-  baseURL: `${apiURL}/article/api/v1`,
+  baseURL: `${apiURL}/blog/api/v1/article`,
   headers: {
     "Content-Type": "application/json",
     "x-api-key": process.env.REACT_APP_X_API_KEY,
